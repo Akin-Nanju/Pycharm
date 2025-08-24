316aabbac81833b73ef224c20c01d36d7d16336b
Akin Nanju <akinnanju9@gmail.com>
2025-08-24 15:24:57 +0545

    THIRD TEST USING COMMAND

---
+++ Thirstest.py
@@ -0,0 +1,11 @@
+# sum_two_numbers.py
+
+# Ask user for two numbers
+num1 = int(input("Enter the first number: "))
+num2 = int(input("Enter the second number: "))
+
+# Calculate sum
+total = num1 + num2
+
+# Print result
+print("The sum of", num1, "and", num2, "is:", total)
